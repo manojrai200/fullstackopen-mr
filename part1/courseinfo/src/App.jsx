10fc1f390d10d1062b2efaf4dae8f35035fff273
manojrai200 <manojrai9801@gmail.com>
2025-08-05 17:15:18 +0545

    debug: fixed no of exercise

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -20,8 +20,8 @@ const Content = ({parts}) => {
   return(
     <div>
         <Part part={parts[0].name} exercises={parts[0].exercises} />
-        <Part part={parts[1].name} exercises={parts[0].exercises} />
-        <Part part={parts[2].name} exercises={parts[0].exercises} />
+        <Part part={parts[1].name} exercises={parts[1].exercises} />
+        <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   )
 }
