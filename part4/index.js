22bc07f9bb633b4f666eba727d1d55c242e46a91
manojrai200 <manojrai9801@gmail.com>
2025-09-03 20:13:29 +0545

    4.4

--- part4/index.js
+++ part4/index.js
@@ -1,45 +1,9 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const { json } = require('express')
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-
-const app = express()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URL
-mongoose.connect(mongoUrl)
-    .then(() => {
-        console.log('connected to MongoDB')
-    }).catch(() => {
-        console.log('error connection to MongoDB', error.message)
-        
-    })
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// app.use(express.json())
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,37 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const dummy = (blogs) => {
+    return 1
+}
 
-const app = express()
+const totalLikes = (blogs) => {
+    return blogs.reduce(
+        (sum, blog) => sum + blog.likes ,
+        0,
+    )
+}
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = {
+    dummy,
+    totalLikes
+}
