735e1a97568bfac53e0786df92310ee16fb7fc53
manojrai200 <manojrai9801@gmail.com>
2025-09-04 18:50:04 +0545

    4.8

--- part4/app.js
+++ part4/app.js
@@ -7,10 +7,10 @@ const blogsRouter = require('./controllers/blogs')
 
 const app = express()
 
-logger.info('connecting to', config.MONGODB_URL)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URL)
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
