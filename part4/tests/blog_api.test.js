435a7cfc5fe6d8d53432c59a77512d26ff9adbe8
manojrai200 <manojrai9801@gmail.com>
2025-09-04 20:10:06 +0545

    4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,15 +1,37 @@
-const { test, after } = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test.only('blogs are returned as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blog = new Blog({
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+  })
+
+  await blog.save()
+})
+
+test('blogs are returned as json', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('blog posts have unique identifier property named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+
+  assert.ok(blog.id, 'id property is defined')
+  assert.strictEqual(blog._id, undefined)
 })
 
 after(async () => {
