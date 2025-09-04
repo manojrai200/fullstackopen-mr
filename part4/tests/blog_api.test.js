bf8918f1f851ff9e63b31f6b5170141a50b08502
manojrai200 <manojrai9801@gmail.com>
2025-09-04 20:56:17 +0545

    4.12

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -7,15 +7,18 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  const blog = new Blog({
+const initialBlogs = [
+  {
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
     likes: 5,
-  })
+  }
+]
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blog = new Blog(initialBlogs[0])
   await blog.save()
 })
 
@@ -32,6 +35,61 @@ test('blog posts have unique identifier property named id', async () => {
 
   assert.ok(blog.id, 'id property is defined')
   assert.strictEqual(blog._id, undefined)
+})
+
+test('verifies HTTP POST request successfully creates a new blog post', async () => {
+  const newBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initialBlogs.length + 1)
+})
+
+test('verifies that if the likes property is missing from the request, it will default to the value 0', async () => {
+  const newBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/"
+  }
+
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    // console.log(response.body)
+    const blog = response.body[1]
+    assert.strictEqual(blog.likes, 0)
+})
+
+test('bolg without title and url is not added', async () => {
+  const newBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    likes: 10
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
 after(async () => {
