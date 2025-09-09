f6a8be4b8f99e871daf04a2314577e08dfc3a6bd
manojrai200 <manojrai9801@gmail.com>
2025-09-09 19:13:08 +0545

    4.19

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const logiinRouter = require('./controllers/login')
 
 const app = express()
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', logiinRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
