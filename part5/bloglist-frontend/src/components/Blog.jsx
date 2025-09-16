40848e2537cdd91d05216e4056500636425f7fc8
manojrai200 <manojrai9801@gmail.com>
2025-09-16 16:36:46 +0545

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showBlog, setShowBlog] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleShow = () => {
+    setShowBlog(!showBlog)
+  }
+  return (
+    <div style={blogStyle}>
+      {!showBlog && <div>
+        {blog.title} {blog.author} <button onClick={handleShow}>view</button>
+      </div>}
+      {showBlog && <div>
+        {blog.title} <button onClick={handleShow}>hide</button> <br />
+        {blog.url} <br />
+        {blog.likes} <button>like</button> <br />
+        {blog.author} <br />
+      </div>}
+  </div>
+)}
 
 export default Blog
