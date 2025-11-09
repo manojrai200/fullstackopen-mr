584121f182988df849eab9669b49c8d25fad50d9
manojrai200 <manojrai9801@gmail.com>
2025-11-09 21:46:45 +0545

    6.10

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,58 +1,48 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0)
+const anecdotesAtStart = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+];
+
+const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
-  }
-}
+    votes: 0,
+  };
+};
 
-export const addVote = (id) => {
-  return{
-    type: "ADD_VOTE",
-    payload: {
-      id: id
-    }
-  }
-}
+const initialState = anecdotesAtStart.map(asObject);
 
-export const creatAnecdote = (content) => {
-  return{
-    type: "NEW_ANECDOTE",
-    payload: {
-      content,
-      votes: 0,
-      id: getId()
-    }
-  }
-}
+const anecdoteSlice = createSlice({
+  name: "anecdotes",
+  initialState,
+  reducers: {
+    creatAnecdote(state, action) {
+      const content = action.payload;
+      state.push({
+        content,
+        vote: 0,
+        id: getId(),
+      });
+    },
+    addVote(state, action) {
+      const id = action.payload;
+      const voteToAdd = state.find((a) => a.id === id);
+      const changedVote = { ...voteToAdd, votes: voteToAdd.votes + 1 };
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedVote
+      );
+    },
+  },
+});
 
-const initialState = anecdotesAtStart.map(asObject)
-
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-  switch(action.type){
-    case "ADD_VOTE":{
-      const id = action.payload.id
-      const voteToAdd = state.find(a => a.id === id)
-      const changedVote = {...voteToAdd, votes: voteToAdd.votes + 1}
-      return state.map(anecdote => anecdote.id !== id ? anecdote : changedVote)
-    }
-    case "NEW_ANECDOTE":
-      return state.concat(action.payload)
-  }
-  return state
-}
-
-export default reducer
+export const { creatAnecdote, addVote } = anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
