b8581f4a6c21a5e13882fb3300d961cddc643bde
manojrai200 <manojrai9801@gmail.com>
2025-11-10 10:39:28 +0545

    6.16

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,8 +1,7 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { showNotification } from "../reducers/notificationReducer";
-import anecdoteService from "../services/anedotes";
-
+import anecdoteService from '../services/anedotes'
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
 
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anedotes";
 
 // const anecdotesAtStart = [
 //   "If it hurts, do it more often",
@@ -42,5 +43,15 @@ const anecdoteSlice = createSlice({
   },
 });
 
-export const { setAnecdotes, createAnecdote, addVote } = anecdoteSlice.actions;
+const { setAnecdotes } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export const { createAnecdote, addVote } = anecdoteSlice.actions;
+
 export default anecdoteSlice.reducer;
